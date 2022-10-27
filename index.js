2125ac3eec2c93a554d7dd598f3bc792836d7aeb
Sazz07 <sazzad.hossain882@gmail.com>
2022-10-27 15:00:48 +0600

    catagories data added

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/catagories.json');
+
 app.get('/', (req, res) => {
     res.send('Courses Api Running');
 });
 
+app.get('/courses-categories', (req, res) => {
+    res.send(categories);
+});
 
 app.listen(port, () => {
     console.log('Sazz.Academy Courses Server Running on port', port);
-});
+})
