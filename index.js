d0347f14c4f4460ff15d2479c040c4a437e7a412
Sazz07 <sazzad.hossain882@gmail.com>
2022-10-27 15:06:27 +0600

    Server site settings complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/catagories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Courses Api Running');
@@ -13,6 +14,27 @@ app.get('/', (req, res) => {
 
 app.get('/courses-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(n => n._id === id);
+    res.send(selectedCourses);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(courses);
+    }
+    else {
+        const category_courses = courses.filter(n => n.category_id === id);
+        res.send(category_courses);
+    }
 });
 
 app.listen(port, () => {
